d8ae9f4cf30ab4c5d7dc4b009b49dc46782cf89e
FatThompson <matthewbiggiethompson@gmail.com>
2016-10-05 14:30:30 -0400

    Adding the GGCMaps

---
+++ Android/GGCmaps/app/src/main/java/mthompson/ggc/edu/ggcmaps/MainActivity.java
@@ -0,0 +1,28 @@
+package mthompson.ggc.edu.ggcmaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void openMaps(){
+
+        Button maps = (Button) findViewById(R.id.btnMaps);
+
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+            }
+        });
+    }
+}
