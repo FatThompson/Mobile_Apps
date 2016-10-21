65810bde2692081648adbb206a7972cd0ba217d4
FatThompson <matthewbiggiethompson@gmail.com>
2016-10-20 20:13:10 -0400

    Working on the homework.

--- Android/Colors/app/src/main/java/edu/ggc/matthew/colors/MainActivity.java
+++ Android/Colors/app/src/main/java/edu/ggc/matthew/colors/MainActivity.java
@@ -2,12 +2,39 @@ package edu.ggc.matthew.colors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    final private String TAG = "Colors.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SeekBar alphaSeek = (SeekBar) findViewById(R.id.alphaSeekBar);
+        SeekBarListener(alphaSeek);
+    }
+
+    public void SeekBarListener(SeekBar mySeekBar) {
+        mySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i(TAG, "Progress Changed. It is at " + progress*2.55);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
