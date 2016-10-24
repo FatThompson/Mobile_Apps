e8fc05fd649f536f9425ad6d159ad90229dd3320
FatThompson <matthewbiggiethompson@gmail.com>
2016-10-24 16:15:42 -0400

    Added Hex Conversion.

--- Android/Colors/app/src/main/java/edu/ggc/matthew/colors/MainActivity.java
+++ Android/Colors/app/src/main/java/edu/ggc/matthew/colors/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.ggc.matthew.colors;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     final private String TAG = "Colors.MainActivity";
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         SeekBarListener(blueSeek);
     }
 
-    public void SeekBarListener(SeekBar mySeekBar) {
+    private void SeekBarListener(SeekBar mySeekBar) {
         //SWAY is a movement to a number = 15^2 sothat I can
         //MOD the result to a HEX value.
         //In theory
@@ -59,21 +62,58 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void changeColorView(){
+    private void changeColorView(){
 
         final SeekBar alphaSeek = (SeekBar) findViewById(R.id.alphaSeekBar);
         final SeekBar redSeek = (SeekBar) findViewById(R.id.redSeekBar);
         final SeekBar greenSeek = (SeekBar) findViewById(R.id.greenSeekBar);
         final SeekBar blueSeek = (SeekBar) findViewById(R.id.blueSeekBar);
+
         //from here, gather the progress and convert the int val to 255 multiple
         //Then we can modify page from that.
         int alphaProgressShift = (int) Math.round(alphaSeek.getProgress()*SWAY);
-        Log.i(TAG, "Progress Changed. It is at " + alphaProgressShift);
+        Log.i(TAG, "Alpha Progress Changed. It is at " + alphaProgressShift);
         int redProgressShift = (int) Math.round(redSeek.getProgress()*SWAY);
-        Log.i(TAG, "Progress Changed. It is at " + redProgressShift);
+        Log.i(TAG, "Red Progress Changed. It is at " + redProgressShift);
         int greenProgressShift = (int) Math.round(greenSeek.getProgress()*SWAY);
-        Log.i(TAG, "Progress Changed. It is at " + greenProgressShift);
+        Log.i(TAG, "Green Progress Changed. It is at " + greenProgressShift);
         int blueProgressShift = (int) Math.round(blueSeek.getProgress()*SWAY);
-        Log.i(TAG, "Progress Changed. It is at " + blueProgressShift);
+        Log.i(TAG, "Blue Progress Changed. It is at " + blueProgressShift);
+
+        //change the text of the text frame color
+        final TextView colorDumpTV= (TextView) findViewById(R.id.colorDumpTextView);
+        String allHex = convertAllToHex(
+                alphaProgressShift,redProgressShift,
+                greenProgressShift,blueProgressShift);
+        colorDumpTV.setText(allHex);
+        Log.i(TAG, "Color: " + allHex );
+
+        //Check alpha if is to light
+        final TextView alphaWarn = (TextView) findViewById(R.id.warnAlphaTooLow);
+        if(alphaProgressShift<50) {
+            alphaProgressShift = 255;
+            alphaWarn.setVisibility(View.VISIBLE);
+            Log.w(TAG,"WARN: Alpha too light, changing to FF");
+            String alphaWarnText = getString(R.string.Alpha_Warn_Text);
+            alphaWarnText = alphaWarnText + "\nReal Color Shown is: "+convertAllToHex(
+                    alphaProgressShift,redProgressShift,
+                    greenProgressShift,blueProgressShift);
+            alphaWarn.setText(alphaWarnText);
+        }
+        else alphaWarn.setVisibility(View.INVISIBLE);
+
+        colorDumpTV.setTextColor(Color.argb(alphaProgressShift,redProgressShift,greenProgressShift,blueProgressShift));
+
+
+
+    }
+    private String convertAllToHex(int avalue,int rvalue,int gvalue,int bvalue){
+        String hex = "#" + convertToHex(avalue)+convertToHex(rvalue)+convertToHex(gvalue)+convertToHex(bvalue);
+        return hex;
+    }
+    private String convertToHex(int value){
+        String hex =  Integer.toHexString(value);
+        if(value < 16) hex = "0"+hex;
+        return hex;
     }
 }
