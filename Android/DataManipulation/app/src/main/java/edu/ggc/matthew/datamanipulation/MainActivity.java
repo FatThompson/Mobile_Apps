752c5999519beabc13eac155cd8a1b1a3b644268
FatThompson <matthewbiggiethompson@gmail.com>
2016-11-24 12:44:30 -0500

    Added some buttons, and now am creating clearing methods. Will create add methods before creating leapyear method.

--- Android/DataManipulation/app/src/main/java/edu/ggc/matthew/datamanipulation/MainActivity.java
+++ Android/DataManipulation/app/src/main/java/edu/ggc/matthew/datamanipulation/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String PREFS_NAME = "MyPrefsFile";
     public static final String LIST_NAME = "list";
     public ArrayList<String> data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Restore preferences
         SharedPreferences sharedData = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        boolean silent = sharedData.getBoolean("silentMode", false);
-//        setSilent(silent);
         data = new ArrayList(sharedData.getStringSet(LIST_NAME,null));
-
-        //convert to arraylist and overirght with more data. (simi inifinte)
         Log.i(TAG,data.toString());
-
+        displayData();
 
     }
 
@@ -38,13 +38,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * saves the data to a shared prefrence
+     * saves the data to a shared preference
+     * this is from the Android API
      */
     public void saveData(){
         // We need an Editor object to make preference changes.
         // All objects are from android.context.Context
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = settings.edit();
+        SharedPreferences prefData = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefData.edit();
         editor.putStringSet(LIST_NAME,new HashSet<String>(data));
 
         // Commit the edits!
@@ -52,11 +53,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Addes data to the array list then saves.
+     * Adds data to the array list then saves.
      * @param dataStr
      */
-    public void addData(String dataStr){
+    private void addData(String dataStr){
         data.add(dataStr);
         saveData();
+        displayData();
+    }
+
+    /**
+     * removes all the data
+     */
+    public void clearData(){
+        data.clear();
+        saveData();
+        displayData();
+    }
+    public void clearData(View view){clearData();}
+
+    /**
+     *  displays the data to the screen
+     */
+    private void displayData(){
+        ListView listView = (ListView) findViewById(R.id.listViewYears);
+        listView.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data));
     }
 }
